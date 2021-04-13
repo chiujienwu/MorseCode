89d85bd2a0922ee3c52094f7612a0a2da417c092
chiujienwu <jchiu1@my.wctc.edu>
2021-04-12 20:04:03 -0500

    Add project files.

---
+++ MorseCode/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MorseCode
+{
+    public partial class MainPage : ContentPage
+    {
+        public string output;
+        public string morse;
+        public string input;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void But_Dot (object sender, EventArgs e)
+        {
+            input += ((Button)sender).Text;
+
+            Output_Morse.Text += input;
+        }
+
+        public void But_Dash(object sender, EventArgs e)
+        {
+            input += ((Button)sender).Text;
+
+            Output_Morse.Text += input;
+        }
+
+        public void But_Space(object sender, EventArgs e)
+        {
+
+            try
+            {
+                char letter = Morse.MorseCoder(input);
+                Output_Letter.Text += letter;
+                Output_Morse.Text += " ";
+                input = "";
+
+            } catch (Exception)
+            {
+                DisplayAlert("Error Occurred", "Oops" , "OK");
+            }
+        }
+    }
+}
